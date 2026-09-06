14db0e1353ff27c1d2dd010c5161a88aec596bf1
Dhanashri vani <dhanashrivani2005@gmail.com>
2026-09-06 19:06:34 +0530

    Finished changes

---
+++ src/components/footer.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+def footer_home():
+
+    logo_url = "https://i.ibb.cp/YTYGn5qv/logo.png"
+
+    st.markdown(f"""
+    <div style="margin-tpo:2rem; display:flex; gap:6px; justyfy-content:center;items-align:center">
+    <p style = "font-weight:bold ; color:white;">Created by <br> DHANASHRI VANI</p>
+
+    </div>
+
+                """, unsafe_allow_html=True
+    
+    )
+
+def footer_dashboard():
+
+    logo_url = "https://i.ibb.cp/YTYGn5qv/logo.png"
+
+    st.markdown(f"""
+    <div style="margin-tpo:2rem; display:flex; gap:6px; justyfy-content:center;items-align:center">
+    <p style = "font-weight:bold ; color:Black;">Created by <br> DHANASHRI VANI</p>
+
+    </div>
+
+                """, unsafe_allow_html=True
+    
+    )    
